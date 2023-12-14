796ee203c363cc231e882a62a42951f0834e6f7c
Christian Haendel <example@example.com>
2023-12-15 00:30:52 +0200

    Week 6

---
+++ Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] fruitBasket = { "apples", "bananas", "oranges" };
+Console.WriteLine($"There are {fruitBasket.Length} fruits in the basket.");
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+
+fruitBasket[0] = "pine-apples";
+fruitBasket[2] = "peaches";
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Enter your firstname:");
+string username = Console.ReadLine().Trim();
+
+Console.WriteLine($"Your name has {username.Length} letters.");
+Console.WriteLine($"The last letter in your name is: {username[username.Length - 1]}.");
+
+//a- counter
+
+int counter = 0;
+
+for (int i = 0; i < username.Length; i++)
+{
+    if (username[i] =='a')
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine($"Your name has {counter} 'a'-letters.");
+
+string updatedUserName = username.Replace('a','4').Replace('e','3');
+Console.WriteLine($"The updated username is {updatedUserName}.");
